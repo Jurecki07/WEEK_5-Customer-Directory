4740eabd7428f6e8c05fc98a4f55a3d64fa7ab4c
Tyler Jurecki <jurecki07@yahoo.com>
2017-08-15 14:06:56 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.port || 3000;
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache');
+
+app.get("/data.js", function (request, response){
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function () {
+    console.log('server is running ${port} ');
+
+
+});
